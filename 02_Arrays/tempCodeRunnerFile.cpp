1f17e951e9fe074a7f1123061b4177e1e475a47c
Deepak Ramani <deepakramani65@gmail.com>
2024-12-26 18:38:06 +0530

    settings updated

--- 02_Arrays/tempCodeRunnerFile.cpp
+++ 02_Arrays/tempCodeRunnerFile.cpp
@@ -1,3 +1,11 @@
-if(nums[i] < smallest) {
-            //     smallest = nums[i];
-            // }
+    // int arr[] = {4, 2, 7, 8, 1, 2, 5};
+    // int sz = 7;
+
+    // reverseArray(arr, sz);
+
+    // // printing array after reverse
+    // for (int i = 0; i < sz; i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+    // cout << endl;
