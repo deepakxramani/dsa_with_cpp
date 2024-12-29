9abff2edd55846f5c360f969e3c5b9a43bf7f273
Deepak Ramani <deepakramani65@gmail.com>
2024-12-29 14:09:03 +0530

    settings updated

--- 02_Arrays/tempCodeRunnerFile.cpp
+++ 02_Arrays/tempCodeRunnerFile.cpp
@@ -1,11 +1,1 @@
-    // int arr[] = {4, 2, 7, 8, 1, 2, 5};
-    // int sz = 7;
-
-    // reverseArray(arr, sz);
-
-    // // printing array after reverse
-    // for (int i = 0; i < sz; i++)
-    // {
-    //     cout << arr[i] << " ";
-    // }
-    // cout << endl;
+ return 0;
